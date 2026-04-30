0ef17aa4e520fcf27909f7e9477b45c1f68d65fb
student <student>
2026-04-30 19:34:22 +0900

    ウェーブ追加しました

--- Assets/ICHIKAWAKUSAMUSHIRIFILE/Script/SpawnManager.cs
+++ Assets/ICHIKAWAKUSAMUSHIRIFILE/Script/SpawnManager.cs
@@ -1,22 +1,73 @@
+using TMPro;
 using UnityEngine;
+using System.Collections;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject prefab;       // 全Spawnerが出すプレハブ
-    [Range(0f, 1f)]
-    public float spawnChance = 0.3f; // 出現確率（親が管理）
+    public GameObject prefab;
+    public float spawnChance = 0.3f; //　スポーン確率
+    public float timeBetweenWaves = 10f; //　ウェーブの時間
+
+    public TextMeshProUGUI waveText;
+    public TextMeshProUGUI timerText;
 
     private Spawner[] spawners;
+    private int wave = 0;
 
     void Start()
     {
-        // 子オブジェクトの Spawner を全部取得
         spawners = GetComponentsInChildren<Spawner>();
+        StartCoroutine(WaveLoop());
+    }
 
-        // 全Spawnerに確率判定を実行
+    //　前のウェーブの草を消すやつ
+    void ClearEnemies()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Grass");
+
+        foreach (var enemy in enemies)
+        {
+            Destroy(enemy);
+        }
+    }
+
+    IEnumerator WaveLoop()
+    {
+        while (true)
+        {
+            wave++;
+            UpdateWaveUI();
+
+            ClearEnemies();//　草削除呼
+
+            RunWave();
+
+            float timer = timeBetweenWaves;
+
+            while (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                UpdateTimerUI(timer);
+                yield return null;
+            }
+        }
+    }
+
+    void RunWave()
+    {
         foreach (var spawner in spawners)
         {
             spawner.TrySpawn(spawnChance, prefab);
         }
+    }
+
+    void UpdateWaveUI()
+    {
+        waveText.text = "Wave: " + wave;
+    }
+
+    void UpdateTimerUI(float time)
+    {
+        timerText.text = "Next: " + Mathf.Ceil(time) + "s";
     }
 }

14167e03208d239bf4a340d34f8a53fc85b991b9
student   ys <student  ys>
2026-05-09 18:00:47 +0900

    作業追加　リザルト

---
+++ Assets/saitou/script/Title.cs
@@ -0,0 +1,13 @@
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+
+//public class Title : MonoBehaviour
+//{
+//    // ƒ{ƒ^ƒ“‚ª‰Ÿ‚³‚ê‚½Žž
+//    public void BackTitle()
+//    {
+//       // SceneManager.LoadScene("TitleScene");
+//    }
+//}
+//}

b437517ab9494e20276b933ceb47de28cd9b1692
kyo <>
2026-05-22 18:16:52 +0900

    タイトルと武器選択画面ボタン追加、シーン移行もできました

--- Assets/kyosuke/Script/NO.cs
+++ Assets/kyosuke/Script/NO.cs
@@ -5,8 +5,8 @@ public class No : MonoBehaviour
     public GameObject weaponSelectUI;
     public GameObject confirmWindow;
     public GameObject lockWindow;
-
-    private void OnMouseDown()
+    
+    public void OnMouseDown()
     {
         confirmWindow.SetActive(false);
         lockWindow.SetActive(false);
--- Assets/kyosuke/Script/START.cs
+++ Assets/kyosuke/Script/START.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    private void OnMouseDown()
+    public void OnMouseDown()
     {
         SceneManager.LoadScene("Game Scene");
     }

630ad9635bea617365c8a88f5a1784cf506a32d2
yuna <>
2026-05-23 16:53:50 +0900

    ゴールと微調整01

---
+++ Assets/saitou/script/Skillpoint.cs
@@ -0,0 +1,11 @@
+//using UnityEngine;
+//using TMPro;
+//public class Skillpoint : MonoBehaviour
+//{
+//    public TextMeshProUGUI resultGrassText;
+//    void Start()
+//    {
+//        int grass = PlayerPrefs.GetInt("SkillPoint", 0);
+//        resultGrassText.text = grass.ToString();
+//    }
+//}

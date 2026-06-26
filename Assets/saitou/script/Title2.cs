ca180f68fe87b4fdf48ce5a8d2698b70a44780d4
yuna <a>
2026-06-26 16:31:08 +0900

    ゴール

--- Assets/saitou/script/Title2.cs
+++ Assets/saitou/script/Title2.cs
@@ -7,6 +7,13 @@ public class Title2 : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip clickSE;
 
+    Vector3 originalScale;
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
     public void Title2Down()
     {
         StartCoroutine(ChangeScene());
@@ -14,9 +21,17 @@ public class Title2 : MonoBehaviour
 
     IEnumerator ChangeScene()
     {
+        // âüÇ≥ÇÍÇΩââèo
+        transform.localScale = originalScale * 0.9f;
+
         audioSource.PlayOneShot(clickSE);
 
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.1f);
+
+        // å≥ÇÃëÂÇ´Ç≥Ç…ñﬂÇ∑
+        transform.localScale = originalScale;
+
+        yield return new WaitForSeconds(0.2f);
 
         SceneManager.LoadScene("TitleScene");
     }

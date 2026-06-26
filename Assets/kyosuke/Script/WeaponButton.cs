8163c00d9cec4740a49098bce71b2f5d4f810d93
kyo <>
2026-06-26 17:47:09 +0900

    武器選択画面つなげる途中

--- Assets/kyosuke/Script/WeaponButton.cs
+++ Assets/kyosuke/Script/WeaponButton.cs
@@ -7,10 +7,42 @@ public class WeaponButton : MonoBehaviour
     public GameObject confirmWindow;
     public GameObject lockWindow;
 
-    public bool unlocked;
+    public bool unlocked01;
+    public bool unlocked02;
+    public bool unlocked03;
+    public bool unlocked04;
+    public bool unlocked05;
+    public bool unlocked06;
+    public bool unlocked07;
+    public bool unlocked08;
+    public bool unlocked09;
 
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip clickSE;
+
+    public PlayerStats playerStats;
+
+    void Update()
+    {
+        if (playerStats.weponUnlock01)
+            unlocked01 = true;
+        if (playerStats.weponUnlock02)
+            unlocked01 = true;
+        if (playerStats.weponUnlock03)
+            unlocked01 = true;
+        if (playerStats.weponUnlock04)
+            unlocked01 = true;
+        if (playerStats.weponUnlock05)
+            unlocked01 = true;
+        if (playerStats.weponUnlock06)
+            unlocked01 = true;
+        if (playerStats.weponUnlock07)
+            unlocked01 = true;
+        if (playerStats.weponUnlock08)
+            unlocked01 = true;
+        if (playerStats.weponUnlock09)
+            unlocked01 = true;
+    }
 
     public void OnMouseDown()
     {
@@ -25,7 +57,71 @@ public class WeaponButton : MonoBehaviour
 
         //weaponSelectUI.SetActive(false);
 
-        if (unlocked)
+        if (unlocked01)
+        {
+            confirmWindow.SetActive(true);
+        }
+        else
+        {
+            lockWindow.SetActive(true);
+        }
+        if (unlocked02)
+        {
+            confirmWindow.SetActive(true);
+        }
+        else
+        {
+            lockWindow.SetActive(true);
+        }
+        if (unlocked03)
+        {
+            confirmWindow.SetActive(true);
+        }
+        else
+        {
+            lockWindow.SetActive(true);
+        }
+        if (unlocked04)
+        {
+            confirmWindow.SetActive(true);
+        }
+        else
+        {
+            lockWindow.SetActive(true);
+        }
+        if (unlocked05)
+        {
+            confirmWindow.SetActive(true);
+        }
+        else
+        {
+            lockWindow.SetActive(true);
+        }
+        if (unlocked06)
+        {
+            confirmWindow.SetActive(true);
+        }
+        else
+        {
+            lockWindow.SetActive(true);
+        }
+        if (unlocked07)
+        {
+            confirmWindow.SetActive(true);
+        }
+        else
+        {
+            lockWindow.SetActive(true);
+        }
+        if (unlocked08)
+        {
+            confirmWindow.SetActive(true);
+        }
+        else
+        {
+            lockWindow.SetActive(true);
+        }
+        if (unlocked09)
         {
             confirmWindow.SetActive(true);
         }

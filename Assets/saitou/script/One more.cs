71c2f74156a60d9ff33d8b3d29c8a81453d0ef7e
yuna <student>
2026-06-29 13:23:26 +0900

    test

--- Assets/saitou/script/One more.cs
+++ Assets/saitou/script/One more.cs
@@ -6,11 +6,14 @@ public class Onemore : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip clickSE;
+
     Vector3 originalScale;
+
     void Start()
     {
         originalScale = transform.localScale;
     }
+
     public void OnMouseDown()
     {
         StartCoroutine(ChangeScene());
@@ -19,14 +22,15 @@ public class Onemore : MonoBehaviour
     IEnumerator ChangeScene()
     {
         transform.localScale = originalScale * 0.9f;
+
         audioSource.PlayOneShot(clickSE);
+
         yield return new WaitForSeconds(0.1f);
 
         transform.localScale = originalScale;
 
-        WaveManager.wave = 0;
         yield return new WaitForSeconds(0.3f);
 
-        SceneManager.LoadScene("WeaponScene");
+        SceneManager.LoadScene("KUSAMUSHIRi");
     }
 }
